e0bb0114944cf19b93c95dddb506d94f7ca94591
sudhanshusharma2010 <sudhanshu.sharma2010@gmail.com>
2019-02-08 06:01:12 +0530

    Word wrap text,orientation change issue fixed.

--- app/src/main/java/com/telstra/poc/ui/FeedsViewHolder.java
+++ app/src/main/java/com/telstra/poc/ui/FeedsViewHolder.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import com.telstra.poc.R;
 import com.telstra.poc.module.GlideApp;
@@ -17,6 +18,10 @@ import butterknife.ButterKnife;
 import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class FeedsViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.feed_relative)
+    RelativeLayout mRelativeLayout;
+
     @BindView(R.id.feed_title)
     AppCompatTextView mTitle;
 
@@ -40,8 +45,24 @@ public class FeedsViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(Row rowData) {
         this.rowData = rowData;
-        mTitle.setText(rowData.getTitle());
-        mDesc.setText(rowData.getDescription());
+        if(rowData.getTitle() != null && !rowData.getTitle().equalsIgnoreCase("")) {
+            mRelativeLayout.setVisibility(View.VISIBLE);
+            mTitle.setVisibility(View.VISIBLE);
+            mTitle.setText(rowData.getTitle());
+        }
+        else {
+            mRelativeLayout.setVisibility(View.GONE);
+            mTitle.setVisibility(View.GONE);
+        }
+        if(rowData.getDescription() != null && !rowData.getDescription().equalsIgnoreCase("")) {
+            mRelativeLayout.setVisibility(View.VISIBLE);
+            mDesc.setVisibility(View.VISIBLE);
+            mDesc.setText(rowData.getDescription());
+        }
+        else {
+            mRelativeLayout.setVisibility(View.GONE);
+            mDesc.setVisibility(View.GONE);
+        }
 
         GlideApp.with(context)
                 .load(rowData.getImageHref())
